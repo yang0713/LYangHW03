36ce3f6e88ab0419afa92a422a82c4c7f958edd6
Liuke Yang <liukeyang@macbook-pro.local>
2017-02-02 16:37:36 -0500

    Initial Commit

---
+++ $125 Million App/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  $125 Million App
+//
+//  Created by Liuke Yang on 2/2/17.
+//  Copyright © 2017 Liuke Yang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
